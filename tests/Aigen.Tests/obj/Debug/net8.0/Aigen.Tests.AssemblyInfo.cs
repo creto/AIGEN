3f4b44e5c4955e6b3c80c48e9a4e32539e4b26be
OSCAR CORTES <oscar.mauricio.cortes@gmail.com>
2026-03-06 08:36:01 -0500

    semana 6 ajustando 2errores

--- tests/Aigen.Tests/obj/Debug/net8.0/Aigen.Tests.AssemblyInfo.cs
+++ tests/Aigen.Tests/obj/Debug/net8.0/Aigen.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("AIGEN Tests - Tests unitarios e integracion")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5caa4e1cd1790484cb84efa3a5fcecc2861d3858")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+238cc79b4803ed9d3f4f7fff0df519a5b22caf04")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aigen.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aigen.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
